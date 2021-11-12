5a198499ecbcff50ef9c72afe37736cb29fcb9b9
Jovana Bulatovic <bulatovicj07@gmail.com>
2021-11-12 18:21:52 +0100

    Add project files.

---
+++ threadapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace threadapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfThreads = 100;
+            int counter = 0;
+
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                counter++;
+                Thread t = new Thread(() =>
+                {
+                    Thread.Sleep(1000);
+                });
+                t.Start();
+            }
+            Console.WriteLine($"Main thread {Thread.CurrentThread.ManagedThreadId} and counter {counter}");
+        }
+    }
+}
